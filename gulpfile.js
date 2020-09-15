a9bf18d288b5aa831d9275895aa0d0f49f5013dd
jimwu <jimwu979@gmail.com>
2020-09-15 16:57:24 +0800

    [gulpfile.js] update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,45 @@
 let gulp = require('gulp');
 let less = require('gulp-less');
 let cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
-gulp.task('compileTo_CSS', function () {
-    return gulp.src('less/dashboard/*.less')
+
+gulp.task('build_CSS', function (done) {
+    
+    gulp.src('src/back/less/*.less')
         .pipe(less())
-        // .pipe(cleanCSS({}))
-        .pipe(gulp.dest('./final/dashboard/css/'));
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/back/css/'));
+        
+    gulp.src('src/front/less/*.less')
+        .pipe(less())
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/front/css/'));
+        
+    done();
 });
+
+gulp.task('build_JS', function(done){
+    
+    gulp.src('src/back/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/back/js/'));
+        
+    gulp.src('src/front/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/front/js/'));
+        
+    done();
+});
+
 gulp.task('default',function(){
-    return gulp.watch('less/dashboard/*.less',gulp.series('compileTo_CSS'));
+    gulp.watch('src/**/less/*.less',gulp.series('build_CSS'));
+    gulp.watch('src/**/js/*.js',gulp.series('build_JS'));
 });
