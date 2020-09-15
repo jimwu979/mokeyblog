2b7c52fa0c4c201c7713cd2ffb6745f26b508730
jimwu <jimwu979@gmail.com>
2020-09-15 17:52:45 +0800

    clean image

--- dist/front/component/endOfBody.php
+++ dist/front/component/endOfBody.php
@@ -1,5 +1,12 @@
 <header>
     <div class="container">
+        <img src="img/logo.jpg">
+        <nav>
+            <a href="index.php">首頁</a>
+            <a href="blog_list.php">部落格</a>
+            <a href="member.php">團隊成員</a>
+            <a href="connect.php">聯絡我們</a>
+        </nav>
     </div>
 </header>
 <footer id="footer">
